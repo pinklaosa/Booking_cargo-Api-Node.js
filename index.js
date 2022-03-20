34df3071a5d1faeb5da9c8f65c4879238e4bddb5
tidekung24350 <rps53009@hotmail.com>
2022-03-20 21:57:06 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,114 @@
+const express = require("express");
+const session = require("express-session");
+const mysql = require("mysql");
+const path = require("path");
+const cors = require("cors");
+const bcrypt = require("bcrypt");
+var jwt = require("jsonwebtoken");
+const saltRounds = 10;
+const port = 3001;
+
+const app = express();
+app.use(cors());
+
+app.use(
+  session({
+    secret: "secret",
+    resave: true,
+    saveUninitialized: true,
+  })
+);
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "static")));
+
+const connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "cargo",
+});
+
+app.get("/test", (req, res) => {
+  res.send({ status: "200" });
+});
+
+app.post("/registed", (req, res) => {
+  const userId = req.body.userId;
+  const firstName = req.body.firstname;
+  const lastName = req.body.lastname;
+  const phoneNum = req.body.phone;
+  const email = req.body.email;
+  const password = req.body.password;
+  const confpassword = req.body.confpassword;
+
+  if (password !== confpassword) {
+    return res.status(400).send({
+      status: 400,
+      msg: "Password and Confrim Password do not match",
+    });
+  } else {
+    bcrypt.hash(password, saltRounds, function (err, hash) {
+      if (err) {
+        console.log(err);
+      }
+      if (hash) {
+        connection.query(
+          "INSERT INTO account (userId , password) VALUES (?,?)",
+          [userId, hash],
+          (err, result) => {
+            if (err) {
+              console.log(err);
+            }
+            if (result) {
+              res.send({ status: 200, msg: "Registeration Successfully" });
+            }
+          }
+        );
+        connection.query(
+          "INSERT INTO account_info (userId , firstName , lastName , email , phoneNum) VALUES (?,?,?,?,?)",
+          [userId, firstName, lastName, email, phoneNum],
+          (err, result) => {}
+        );
+      }
+    });
+  }
+  //   console.log(req.body);
+});
+
+app.post("/logged", (req, res) => {
+  const userId = req.body.userId;
+  const password = req.body.password;
+
+  if (userId && password) {
+    connection.query(
+      "SELECT * FROM account WHERE userId = ?",
+      [userId],
+      (err, users) => {
+        if (err) {
+          res.send({ err: err });
+        }
+        if (users.length > 0) {
+          bcrypt.compare(password, users[0].password, function (err, result) {
+            if (result) {
+              res.json({ status: 200, msg: "Logging successfully !" });
+            } else {
+              res.json({
+                status: 404,
+                msg: "Wrong email/password combination!",
+              });
+            }
+          });
+        } else {
+          res.send({ status: 404, msg: "Wrong email combination!" });
+        }
+      }
+    );
+  } else {
+    res.send("Please enter Username and Password!");
+  }
+});
+
+app.listen(port, () => {
+  console.log(`This app listening on port ${port}`);
+});
